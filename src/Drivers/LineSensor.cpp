87dd29ccb72cea752b2dd785259cdeb0ec8e9f9e
jorknor <jor123456789rit@gmail.com>
2025-11-04 13:19:23 +0100

    Fix syntax errors in LineSensor.cpp

--- src/Drivers/LineSensor.cpp
+++ src/Drivers/LineSensor.cpp
@@ -1,13 +1,15 @@
 #include "LineSensor.h"
 int lineSensors [4] ={0};
 void lineSensorInit(){
-  DDRC &= ~(HLS_PIN|MLS_PIN|MRS_PIN|HMS_PIN)
+  DDRC &= ~(HLS_PIN|MLS_PIN|MRS_PIN|HMS_PIN);
+}
 void lineSensorUpdate(){
   lineSensors[0] = PINC & HLS_PIN;
   lineSensors[1] = PINC & MLS_PIN;
   lineSensors[2] = PINC & MRS_PIN;
   lineSensors[3] = PINC & HRS_PIN;
 }
+
 
 
 
