c27bf9dcf8a0b089ce2b48d2138e8584bd05d665
jorknor <jor123456789rit@gmail.com>
2025-11-04 16:15:35 +0100

    Change eepromRead return type to uint8_t

--- src/Util.h
+++ src/Util.h
@@ -4,4 +4,4 @@ extern volatile uint64_t ticks;
 void timerZeroInit();
 void timerZeroUpdate();
 void eepromWrite(uint16_t address, uint8_t data);
-void eepromRead(uint16_t address);
+uint8_t eepromRead(uint16_t address);
