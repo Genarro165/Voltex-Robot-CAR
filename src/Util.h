ed83278a8af0d7b0c1f27014a51aacd51355e998
jorknor <jor123456789rit@gmail.com>
2025-11-04 16:15:05 +0100

    Update EEPROM functions to include address parameter

--- src/Util.h
+++ src/Util.h
@@ -3,5 +3,5 @@ extern volatile uint64_t ticks;
 
 void timerZeroInit();
 void timerZeroUpdate();
-void eepromWrite();
-void eepromRead();
+void eepromWrite(uint16_t address, uint8_t data);
+void eepromRead(uint16_t address);
