b49beeb8b82dd8f7f50eb77079184b2e1676335d
imbluedabr <baldergroen@gmail.com>
2025-11-12 10:09:41 +0100

    fixed missing ticks variable

--- src/Util.cpp
+++ src/Util.cpp
@@ -1,11 +1,14 @@
 #include "Util.h"
 
+volatile uint64_t ticks;
+
 void timerZeroInit(){
   TCCR0A = 0; // normal mode
   TCCR0B = 0;
   TCCR0B |= 0b00000010; // prescaler 8
   TIMSK0 |= 0b00000001; // enable overflow interrupt
   TCNT0 = 0;
+  ticks = 0;
 }
 
 void timerZeroUpdate(){
