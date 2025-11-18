3747b9fe261f6debe7134b158c6094e217c36871
jorknor <jor123456789rit@gmail.com>
2025-11-18 10:42:51 +0100

    Update echo pin definitions for distance sensor

--- src/Drivers/DistanceSensor.h
+++ src/Drivers/DistanceSensor.h
@@ -1,8 +1,8 @@
 #pragma once
 #define PBtrigPin (1<<0)
-#define PDleftEchoPin (1<<3)
-#define PDmiddleEchoPin (1<<4)
-#define PDrightEchoPin (1<<5)
+#define PDleftEchoPin (1<<4)
+#define PDmiddleEchoPin (1<<5)
+#define PDrightEchoPin (1<<6)
 
 extern int leftDistance_cm;
 extern int middleDistance_cm;
