e6f9d57bb53cb70a0cb8ae024c10f5577f92e787
imbluedabr <baldergroen@gmail.com>
2025-11-26 00:25:42 +0100

    small changes

--- src/Drivers/BluetoothDriver.cpp
+++ src/Drivers/BluetoothDriver.cpp
@@ -5,11 +5,12 @@ char bluetoothInputString[BLUETOOTH_BUFFER_SIZE] = {0};
 bool bluetoothStringComplete = false;
 int bluetoothInputSize = 0;
 SoftwareSerial bluetooth(BRX, BTX);
+
 void bluetoothInit() {
   pinMode(BRX, INPUT);
   pinMode(BTX, OUTPUT);
    // Set the baud rate for the SoftwareSerial object
-    bluetooth.begin(9600);
+  bluetooth.begin(9600);
 }
 
 void bluetoothUpdate() {
