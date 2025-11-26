77f31ab80d02c9695de59d52fb96e81476385d0b
imbluedabr <baldergroen@gmail.com>
2025-11-26 10:55:41 +0100

    updated with feater guards

--- src/Drivers/BluetoothDriver.cpp
+++ src/Drivers/BluetoothDriver.cpp
@@ -3,17 +3,18 @@ char bluetoothInputString[BLUETOOTH_BUFFER_SIZE] = {0};
 bool bluetoothStringComplete = false;
 int bluetoothInputSize = 0;
 
-//SoftwareSerial bluetooth(BRX, BTX);
+#ifdef ARDUINO_CORE
+SoftwareSerial bluetooth(BRX, BTX);
 
 void bluetoothInit() {
-  //pinMode(BRX, INPUT);
-  //pinMode(BTX, OUTPUT);
-  // Set the baud rate for the SoftwareSerial object
-  //bluetooth.begin(9600);
+  pinMode(BRX, INPUT);
+  pinMode(BTX, OUTPUT);
+  //Set the baud rate for the SoftwareSerial object
+  bluetooth.begin(9600);
 }
 
 void bluetoothUpdate() {
-  /*while (bluetooth.available()) {
+  while (bluetooth.available()) {
     // Get the new byte
     char inChar = (char)bluetooth.read();
   if (inChar == '\r') {
@@ -27,5 +28,16 @@ void bluetoothUpdate() {
           bluetoothStringComplete = true;
         }
     }
-  }*/
+  }
 }
+#else
+
+void bluetoothInit() {
+
+}
+
+void bluetoothUpdate() {
+
+}
+
+#endif
